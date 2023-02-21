d763a08bb86af2bdc2287f88812f1806ee1b8735
Sharan Reddy <sharansaikonda99@gmail.com>
2023-02-21 09:12:05 -0500

    dockerize application

--- backend/helpers.js
+++ backend/helpers.js
@@ -1,112 +1,133 @@
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt')
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
-const {adminUser, normalUser} = require('./constants')
-const userModal = require('./models/userModel');
-const adminUserModal = require('./models/adminUserModel');
-const s3Helper = require('./s3Helper');
+const { adminUser, normalUser } = require("./constants");
+const userModal = require("./models/userModel");
+const adminUserModal = require("./models/adminUserModel");
+const s3Helper = require("./s3Helper");
 
-require('dotenv').config()
+require("dotenv").config();
 
 let present_tokens = [];
-function getErrorValidationMsg(error){
-    let errors = [];
-    if(!Array.isArray(error)){
-        errors.push(error);
-        return errors;
-    }
-    for (const [key, value] of Object.entries(error)) {
-        errors.push(value['message']);
-    }
+function getErrorValidationMsg(error) {
+  let errors = [];
+  if (!Array.isArray(error)) {
+    errors.push(error);
     return errors;
+  }
+  for (const [key, value] of Object.entries(error)) {
+    errors.push(value["message"]);
+  }
+  return errors;
 }
 
-function generateAccessToken(user){
-    const token = jwt.sign(user.toJSON(),process.env.access_token,{'expiresIn':'10000000m'});
-    present_tokens.push(token);
-    return token;
-
+function generateAccessToken(user) {
+  const token = jwt.sign(
+    user.toJSON(),
+    process.env.GETAPLACE_JWT_ACCESS_TOKEN,
+    { expiresIn: "10000000m" }
+  );
+  present_tokens.push(token);
+  return token;
 }
 
-function validateToken(req,res,next){
-    const header = req.headers['authorization'];
-    if(header == null){
-        res.status(400).json({status:0,"error":"authorization header is missing"});
-        return;
-    }
-    const token = header.split(" ")[1];
-    if (token == null){ // Include present_tokens check
-        res.status(400).json({status:0,"error":"Token not present"});
-        return;
-    }
-    try{
-        const user = jwt.verify(token,process.env.access_token);
-        req.user = user
-        next();
-    }catch(error){
-        res.status(403).json({'status':0,'error':'Invalid Token'})
-    }
+function validateToken(req, res, next) {
+  const header = req.headers["authorization"];
+  if (header == null) {
+    res
+      .status(400)
+      .json({ status: 0, error: "authorization header is missing" });
+    return;
+  }
+  const token = header.split(" ")[1];
+  if (token == null) {
+    // Include present_tokens check
+    res.status(400).json({ status: 0, error: "Token not present" });
+    return;
+  }
+  try {
+    const user = jwt.verify(token, process.env.GETAPLACE_JWT_ACCESS_TOKEN);
+    req.user = user;
+    next();
+  } catch (error) {
+    res.status(403).json({ status: 0, error: "Invalid Token" });
+  }
 }
 
-async function registerUser(req,res, userTypeModal){
-    
-    let body = req.body;
-    if(!(body.name) || !(body.email) || !(body.password)){
-        res.status(400).json({'status':0,'error':"Name Email and Password and Phone Number are required fields"});
-        return;
-    }
-    
-    const user = await userTypeModal.find({'$or':[{'email':body.email},{'phoneNumber':body.phoneNumber}]});
-    
-    if(user.length !=0){
-        res.status(400).json({'status' :0 ,'error' :"Email ID/ Phone Number already Registered"});
-        return;
-    }
-    
-    const newUser = new userTypeModal({
-            name: body.name,
-            email: body.email,
-            password: await bcrypt.hash(body.password.toString(),15),
-            countryCode: body.countryCode,
-            phoneNumber: body.phoneNumber
-
-        })
-    try{
-        await newUser.save();
-    }
-    catch(error){
-        res.status(400).json({'status':0,'error':helpers.getErrorValidationMsg(error.errors)});
-        return;
-    }
-    res.status(201).json({
-        'status':1,
-        'message' : "User Registered Successfully",
-    })
-}
-
-async function logInUser(req,res, userTypeModal){
-    let body = req.body;
-    if(!(body.email) || !(body.password)){
-        res.status(400).json({'status':0,'error':'Both Email and Password are required'});
-        return;
-    }
-    const user = await userTypeModal.findOne({'email':body.email});
-    if(!(user)){
-        res.status(401).json({'status':1,'error':"Email not registered with us. Pl register"});
-        return;
-    }
-    const password = await bcrypt.hash(body.password,15);
-    if(!(await bcrypt.compare(body.password,user.password))){
-        res.status(401).json({'status':0,'error':"Incorrect Password"});
-        return;
-    }
-    const access_token = generateAccessToken(user);
-    res.status(200).json({
-        'status':1,
-        'message': 'Login SuccessFull',
-        'access_token': access_token
+async function registerUser(req, res, userTypeModal) {
+  let body = req.body;
+  if (!body.name || !body.email || !body.password) {
+    res.status(400).json({
+      status: 0,
+      error: "Name Email and Password and Phone Number are required fields",
     });
+    return;
+  }
+
+  const user = await userTypeModal.find({
+    $or: [{ email: body.email }, { phoneNumber: body.phoneNumber }],
+  });
+
+  if (user.length != 0) {
+    res
+      .status(400)
+      .json({ status: 0, error: "Email ID/ Phone Number already Registered" });
+    return;
+  }
+
+  const newUser = new userTypeModal({
+    name: body.name,
+    email: body.email,
+    password: await bcrypt.hash(body.password.toString(), 15),
+    countryCode: body.countryCode,
+    phoneNumber: body.phoneNumber,
+  });
+  try {
+    await newUser.save();
+  } catch (error) {
+    res
+      .status(400)
+      .json({ status: 0, error: helpers.getErrorValidationMsg(error.errors) });
+    return;
+  }
+  res.status(201).json({
+    status: 1,
+    message: "User Registered Successfully",
+  });
 }
 
+async function logInUser(req, res, userTypeModal) {
+  let body = req.body;
+  if (!body.email || !body.password) {
+    res
+      .status(400)
+      .json({ status: 0, error: "Both Email and Password are required" });
+    return;
+  }
+  const user = await userTypeModal.findOne({ email: body.email });
+  if (!user) {
+    res
+      .status(401)
+      .json({ status: 1, error: "Email not registered with us. Pl register" });
+    return;
+  }
+  const password = await bcrypt.hash(body.password, 15);
+  if (!(await bcrypt.compare(body.password, user.password))) {
+    res.status(401).json({ status: 0, error: "Incorrect Password" });
+    return;
+  }
+  const access_token = generateAccessToken(user);
+  res.status(200).json({
+    status: 1,
+    message: "Login SuccessFull",
+    access_token: access_token,
+  });
+}
 
-module.exports = {getErrorValidationMsg,generateAccessToken,validateToken,registerUser,logInUser};
+module.exports = {
+  getErrorValidationMsg,
+  generateAccessToken,
+  validateToken,
+  registerUser,
+  logInUser,
+};
--- backend/s3Helper.js
+++ backend/s3Helper.js
@@ -1,48 +1,52 @@
-const aws = require('aws-sdk');
-require('dotenv').config();
-const uuid = require('uuid');
+const aws = require("aws-sdk");
+require("dotenv").config();
+const uuid = require("uuid");
 
-async function uploadFile(base64Data,fileType,acl="public-read"){
-    const params = {
-        Bucket: process.env.AWS_BUCKET_NAME,
-        Body: Buffer.from(base64Data, 'base64'),
-        Key: uuid.v4(),
-        ContentEncoding: 'base64',
-        ContentType: `${fileType}`,
-        ACL: 'public-read'
-    }  
-    const s3Client = getS3Client();
-    let s3Details;
-    try{
-        s3Details = await (s3Client.upload(params).promise());
-    }
-    catch(error){
-        return {'status' : 0, 'error' : error}
-    }
+async function uploadFile(base64Data, fileType, acl = "public-read") {
+  const params = {
+    Bucket: process.env.GETAPLACE_AWS_BUCKET_NAME,
+    Body: Buffer.from(base64Data, "base64"),
+    Key: uuid.v4(),
+    ContentEncoding: "base64",
+    ContentType: `${fileType}`,
+    ACL: "public-read",
+  };
+  const s3Client = getS3Client();
+  let s3Details;
+  try {
+    s3Details = await s3Client.upload(params).promise();
+  } catch (error) {
+    return { status: 0, error: error };
+  }
 
-    return {'status':1, 's3Details': s3Details};
+  return { status: 1, s3Details: s3Details };
 }
 
-async function uploadImages(images){
-    let s3Images = [];
-    let image,type;
-    for(let i=0; i<images.length;i++){
-        type = images[i].match(/^data:image\/([a-zA-Z]+);base64,/)[1];
-        image = await uploadFile(images[i],`image/${type}`);  
-        if(image.status != 0){
-            s3Images.push(image.s3Details);
-        }
+async function uploadImages(images) {
+  let s3Images = [];
+  let image, type;
+  for (let i = 0; i < images.length; i++) {
+    type = images[i].match(/^data:image\/([a-zA-Z]+);base64,/)[1];
+    image = await uploadFile(images[i], `image/${type}`);
+    if (image.status != 0) {
+      s3Images.push(image.s3Details);
     }
-    if(images.length != s3Images.length){
-        return {'status' : 0, 'error': "Some images were not uploaded"};
-    }
-    return {'status':1, 's3Details':s3Images};
+  }
+  if (images.length != s3Images.length) {
+    return { status: 0, error: "Some images were not uploaded" };
+  }
+  return { status: 1, s3Details: s3Images };
 }
 
-function getS3Client(){
-    const s3Client = new aws.S3({region:process.env.AWS_REGION_NAME,credentials:{accessKeyId:process.env.AWS_ACCESS_KEY,secretAccessKey:process.env.AWS_SECRET_ACCESS_KEY}});
-    return s3Client
-
+function getS3Client() {
+  const s3Client = new aws.S3({
+    region: process.env.GETAPLACE_AWS_REGION_NAME,
+    credentials: {
+      accessKeyId: process.env.GETAPLACE_AWS_ACCESS_KEY,
+      secretAccessKey: process.env.GETAPLACE_AWS_SECRET_ACCESS_KEY,
+    },
+  });
+  return s3Client;
 }
 
-module.exports = {uploadFile, uploadImages}
+module.exports = { uploadFile, uploadImages };
